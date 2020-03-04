4da0866381389b9c4c7f910f03b69693f7b385b8
Ki Jung Kim <kijung.kim@cgius.com>
2020-03-04 11:31:38 -1000

    generated all components needed for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,31 @@ import { MaterialModule } from './modules/material/material.module';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { HomeComponent } from './sections/home/home.component';
+import { ProfileComponent } from './sections/profile/profile.component';
+import { MottosComponent } from './sections/mottos/mottos.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { EducationComponent } from './sections/education/education.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProfileComponent,
+    MottosComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    EducationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserAnimationsModule,
---
+++ src/app/sections/mottos/mottos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mottos',
+  templateUrl: './mottos.component.html',
+  styleUrls: ['./mottos.component.scss']
+})
+export class MottosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

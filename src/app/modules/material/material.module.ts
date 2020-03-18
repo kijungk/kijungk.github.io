621b9197e38821c82bb0d08168e76f412ceec283
Ki Jung Kim <kijung.kim@cgius.com>
2020-03-17 16:15:58 -1000

    imported material modules

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ],
   exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ]
 })
 export class MaterialModule { }
